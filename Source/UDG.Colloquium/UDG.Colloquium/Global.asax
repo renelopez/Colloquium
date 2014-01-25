f3859d18ab776e3319da82a6c5904dbf1261ee75
Rene Lopez <rene.lopez@unosquare.com>
2014-01-24 18:33:29 -0600

    First Initial Commit

---
+++ Source/UDG.Colloquium/UDG.Colloquium/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UDG.Colloquium.MvcApplication" Language="C#" %>
---
+++ Source/UDG.Colloquium/UDG.Colloquium/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UDG.Colloquium.Startup))]
+namespace UDG.Colloquium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
