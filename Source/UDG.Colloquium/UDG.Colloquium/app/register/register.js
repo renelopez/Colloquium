604e35050deec489b123dcf4ba56cd8e5cb9b352
renelopez <rene.lopez.cano@gmail.com>
2014-06-13 19:00:06 -0500

    added Works and Contacts

--- Source/UDG.Colloquium/UDG.Colloquium/app/register/register.js
+++ Source/UDG.Colloquium/UDG.Colloquium/app/register/register.js
@@ -4,9 +4,9 @@
     var controllerId = 'registerController';
 
     angular.module('formApp').controller(controllerId,
-        ['$scope','common', registerController]);
+        ['$scope','common','registerDatacontext', registerController]);
 
-    function registerController($scope, common) {
+    function registerController($scope, common,registerDatacontext) {
         var getLogFn = common.logger.getLogFn;
         var log = getLogFn(controllerId);
         var logInfo = getLogFn(controllerId, "info");
@@ -15,18 +15,57 @@
         var vm = this;
         vm.formData = {};
         vm.processForm = processForm;
-
+        vm.removeSelectedWork = removeSelectedWork;
+        vm.addWork = addWork;
+        vm.removeLastWork = removeLastWork;
+        vm.showCompanyForm = showCompanyForm;
+        vm.hideCompanyForm = hideCompanyForm;
         activate();
 
         
         function activate() {
             common.activateController([], controllerId).then(function() {
                 log("Activated Register View");
+                addWork();
             });
         }
         
         function processForm() {
             
         }
+        
+        function addWork() {
+            registerDatacontext.postWork(formData,{
+                workPosition: vm.workPosition,
+                workDescription: vm.workDescription,
+                workSalary: vm.workSalary,
+                workSalarySchema: vm.workSalarySchema,
+                workBeginDate: vm.workBeginDate,
+                workEndDate: vm.workEndDate,
+                company: vm.company,
+                companyFormVisible: vm.companyFormVisible
+            });
+        }
+        
+        function removeLastWork() {
+            registerDatacontext.removeLastWork(formData);
+        }
+
+        function removeSelectedWork(work) {
+            var selectedWorkToDelete = vm.formData.works[work];
+            registerDatacontext.removeSelectedWork(formData,selectedWorkToDelete);
+        }
+        
+        function showCompanyForm(work) {
+            var works = vm.formData.works;
+            works[works.indexOf(work)].companyFormVisible = true;
+        }
+        
+        function hideCompanyForm(work) {
+            var works = vm.formData.works;
+            works[works.indexOf(work)].companyFormVisible = false;
+        }
+        
+
     }
 })();
---
+++ Source/UDG.Colloquium/UDG.Colloquium/app/services/registerDatacontext.js
@@ -0,0 +1,44 @@
+﻿(function() {
+    'use strict';
+
+    var serviceId = 'registerDatacontext';
+    //angular.module('formApp').factory(serviceId, ['common', 'breeze', 'entityManagerFactory', datacontext]);
+    angular.module('formApp').factory(serviceId, ['common', registerDatacontext]);
+
+    //function datacontext(common, breeze, entityManagerFactory) {
+    function registerDatacontext(common) {
+        var $q = common.$q;
+        //var manager = entityManagerFactory.getEntityManager();
+
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(serviceId);
+        var logError = getLogFn(serviceId, 'error');
+        var logSuccess = getLogFn(serviceId, 'success');
+
+        var service = {
+            postWork: postWork,
+            removeLastWork: removeLastWork,
+            removeSelectedWork:removeSelectedWork
+        };
+
+        return service;
+        
+        function postWork(data,work) {
+            data.works.push(work);
+        }
+        
+        function removeLastWork(data) {
+            data.works.pop();
+        }
+
+        function removeSelectedWork(data,work) {
+            data.works.splice(data.works.indexOf(work), 1);
+            work = null;
+        }
+        
+
+
+    }
+}
+)();
+
